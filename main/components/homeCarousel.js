4afd664a13fd20f9a00ae59a406c1bb2ef5b4c53
installerJOE <justiceigoma@gmail.com>
2021-01-04 23:00:04 +0100

    Slide show cleaner code

--- main/components/homeCarousel.js
+++ main/components/homeCarousel.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import SubCarousel from './subCarousel'
 
 
-
+//array of images making up the carousel
 const slideImages = [
   {
     id: "1",
@@ -18,42 +18,45 @@ const slideImages = [
   }
 ]
 
-
 class HomeCarousel extends Component{
   constructor(){
     super()
     this.state = {
-      isActive: 0
+      isActive: 0,
+      breakInterval: false
     }
     this.handleCarouselMovement = this.handleCarouselMovement.bind(this)
+  }
+
+  componentDidMount(){
+    setInterval(() => this.handleCarouselMovement(true), 2000)
+  }
+
+  pauseAutomation(){
+
+  }
+  playAutomation(){
+    setInterval(() => this.handleCarouselMovement(true), 2000)
   }
 
   handleCarouselMovement(forward){
     this.setState(prev => {
       // bring the next image on the slide show
       if(forward){
-        if(this.state.isActive < (slideImages.length - 1)){
-          return{
-            isActive: prev.isActive + 1
-          }
-        }
-        else{
-          return{
-            isActive: (prev.isActive - prev.isActive)
-          }
+        return{
+          //make sure isActive is always less than number of images in the carousel
+          //as well as greater or equal to index [0]
+          isActive: this.state.isActive < (slideImages.length - 1) ?
+            (prev.isActive + 1) : (prev.isActive - prev.isActive),
         }        
       }
       //bring the previous image in the slide show
       else{
-        if(this.state.isActive === 0){
-          return{
-            isActive: (prev.isActive + (slideImages.length - 1))
-          }
-        }
-        else{
-          return{
-            isActive: prev.isActive - 1
-          }
+        return{
+          //make sure that isActive is always greater or equal to index [0]
+          //while less than the number of images making up the carousel
+          isActive: this.state.isActive === 0 ? 
+            (prev.isActive + (slideImages.length - 1)) : (prev.isActive - 1),
         }
       }
     })
