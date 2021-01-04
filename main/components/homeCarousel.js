7472ccf222a2b69a23a8774372fe4a965719a881
installerJOE <justiceigoma@gmail.com>
2021-01-04 17:37:44 +0100

    Carousel Update

--- main/components/homeCarousel.js
+++ main/components/homeCarousel.js
@@ -1,32 +1,73 @@
 import React, {Component} from 'react'
 import SubCarousel from './subCarousel'
 
+
+
+const slideImages = [
+  {
+    id: "1",
+    url: "./images/slider.jpg"
+  },
+  {
+    id: "2",
+    url: "./images/shoe.jpg"
+  },
+  {
+    id: "3",
+    url: "./images/coffee-mugs.jpg"
+  }
+]
+
+
 class HomeCarousel extends Component{
   constructor(){
     super()
     this.state = {
-        isClicked: false
+      isActive: 0
     }
     this.handleCarouselMovement = this.handleCarouselMovement.bind(this)
   }
 
-  handleCarouselMovement(){
+  handleCarouselMovement(forward){
     this.setState(prev => {
-      return{
-        isClicked: !prev.isClicked
+      // bring the next image on the slide show
+      if(forward){
+        if(this.state.isActive < (slideImages.length - 1)){
+          return{
+            isActive: prev.isActive + 1
+          }
+        }
+        else{
+          return{
+            isActive: (prev.isActive - prev.isActive)
+          }
+        }        
+      }
+      //bring the previous image in the slide show
+      else{
+        if(this.state.isActive === 0){
+          return{
+            isActive: (prev.isActive + (slideImages.length - 1))
+          }
+        }
+        else{
+          return{
+            isActive: prev.isActive - 1
+          }
+        }
       }
     })
-    
   }
 
-  render(){
+  render(){    
+    let activeImg = slideImages[this.state.isActive]
     return(
       <div className="col-12">
-                
-        <SubCarousel
+        <SubCarousel 
+          id={this.state.isActive + 1} 
+          image={activeImg.url} 
           handleClick={this.handleCarouselMovement}
         />
-        
       </div>
     )
   }
--- main/components/subCarousel.js
+++ main/components/subCarousel.js
@@ -1,18 +1,27 @@
 import React from 'react'
 
 function SubCarousel(props){
+  // alert(props.id)
   return(
     <div className="div-carousel">
       {/* Previous Slide button */}
-      <div className="carousel-ctrl" style={{float: "left", left: "30px"}} onClick={()=>props.handleClick()}> 
+      <div 
+        className="carousel-ctrl" 
+        style={{float: "left", left: "30px"}} 
+        onClick={()=>props.handleClick(false)}
+      > 
         &#10094; 
       </div>
       
       {/* Unit Image of the slide */}
-      <img src="./images/slider.jpg" alt="carousel"/>
+      <img src={props.image} alt="carousel"/>
       
       {/* Next slide button */}
-      <div className="carousel-ctrl" style={{float: "right", right: "30px"}} onClick={()=>props.handleClick()}> 
+      <div 
+        className="carousel-ctrl" 
+        style={{float: "right", right: "30px"}} 
+        onClick={()=>props.handleClick(true)}
+      > 
         &#10095; 
       </div>
     </div>
